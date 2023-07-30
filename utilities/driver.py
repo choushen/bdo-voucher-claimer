290c7e6df49fe4b0afa7c8fc4b267518cc58b8cd
choushen <jacob.mckenzie@icloud.com>
2023-07-31 00:20:39 +0100

    driver config

--- utilities/driver.py
+++ utilities/driver.py
@@ -1,21 +1,42 @@
-from selenium import webdriver
 import platform
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
-# Set path to Firefox WebDriver executable
-
+# Check the user's operating system
 if platform.system() == 'Windows':
-    driver_path = 'utilities/geckodriver-windows.exe'
-elif platform.system() == 'MacOS':
-    driver_path = 'utilities/geckodriver-mac'
+    # Set path to Chrome WebDriver executable for Windows
+    driver_path = 'path/to/chromedriver.exe'
+elif platform.system() == 'Darwin' or platform.system().startswith('Mac'):
+    # Set path to Chrome WebDriver executable for Mac
+    driver_path = 'path/to/chromedriver'
+else:
+    # Set path to Chrome WebDriver executable for Linux
+    driver_path = 'path/to/chromedriver'
 
+# Initialize Chrome driver with executable path
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
-
-# Set options for Firefox browser
+# Initialize Firefox driver with executable path
 firefox_options = webdriver.FirefoxOptions()
-firefox_options.headless = True
+firefox_options.add_argument('--headless')
+firefox_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=firefox_options)
 
-# Initialize Firefox driver with options and executable path
-driver = webdriver.Firefox(executable_path=driver_path, options=firefox_options)
+# Check if Firefox is installed on the user's system
+try:
+    firefox_driver.get('about:blank')
+except:
+    firefox_installed = False
+else:
+    firefox_installed = True
+
+# Use the appropriate driver based on the user's system and installed browsers
+if firefox_installed:
+    driver = firefox_driver
+else:
+    driver = chrome_driver
 
 # Use driver to navigate to a website:
 driver.get('https://www.example.com')
