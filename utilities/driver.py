207d60db30de6b1d7ae095bf2953f39714b9bb9b
choushen <jacob.mckenzie@icloud.com>
2023-07-31 00:31:29 +0100

    driver config

--- utilities/driver.py
+++ utilities/driver.py
@@ -1,7 +1,7 @@
 import platform
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 
 # Check the user's operating system
 if platform.system() == 'Windows':
@@ -15,33 +15,15 @@ else:
     driver_path = 'path/to/chromedriver'
 
 # Initialize Chrome driver with executable path
+ChromeDriverManager().install()
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-
-# Initialize Firefox driver with executable path
-firefox_options = webdriver.FirefoxOptions()
-firefox_options.add_argument('--headless')
-firefox_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=firefox_options)
-
-# Check if Firefox is installed on the user's system
-try:
-    firefox_driver.get('about:blank')
-except:
-    firefox_installed = False
-else:
-    firefox_installed = True
-
-# Use the appropriate driver based on the user's system and installed browsers
-if firefox_installed:
-    driver = firefox_driver
-else:
-    driver = chrome_driver
+#chrome_options.add_argument('--headless')
+chrome_driver = webdriver.Chrome(options=chrome_options)
 
 # Use driver to navigate to a website:
-driver.get('https://www.example.com')
+chrome_driver.get('https://www.example.com')
 
 # Do something else...
-
+time.sleep(5)
 # Close driver when finished
-driver.quit()
+chrome_driver.quit()
